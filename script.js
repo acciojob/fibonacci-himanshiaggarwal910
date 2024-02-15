8df29d78e51ae3c5d75da76c72de5db24d9ba27b
himanshiaggarwal910 <141843813+himanshiaggarwal910@users.noreply.github.com>
2024-02-15 14:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+    let first = 0, second = 1;
+    if (num < 3) return num;
+    let ans;
+
+    for (let i = 3; i <= num; i++) {
+        ans = first + second;
+        first = second;
+        second = ans;
+    }
+
+    return ans;
+}
+
+module.exports = fibonacci;
+
+	
 	let n=prompt("enter the number");
 	let num=parseInt(n);
 	let first=0,second=1;
